6789acaae2291f332c73942c068fe663b2408f3f
gengyuanzhang <gengyuanmax@gmail.com>
2020-02-21 17:20:34 +0100

    FaceRecognition Component

--- ROSBridge/smartsoft/build/CMakeFiles/ROSBridge_distclean.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/ROSBridge_distclean"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/ROSBridge_distclean.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
--- ROSBridge/smartsoft/build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/rosmosys/SOFTWARE/smartsoft-ace-mdsd-v3/repos/PerceptionComponents/ROSBridge/smartsoft
-# Build directory: /home/rosmosys/SOFTWARE/smartsoft-ace-mdsd-v3/repos/PerceptionComponents/ROSBridge/smartsoft/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
